be3ee658a2b2ccd533e0393c863b3754012793db
9woody5 <woojin958@gmail.com>
2023-10-24 01:17:32 +0900

    feat: Project settings

---
+++ client/blog-app/src/App.tsx
@@ -0,0 +1,12 @@
+import styles from "./App.module.scss";
+
+function App() {
+  return (
+    <div className={styles.hello}>
+      Hello React with Vite + SASS
+      <div className={styles.hiagain}>hi again!</div>
+    </div>
+  );
+}
+
+export default App;
